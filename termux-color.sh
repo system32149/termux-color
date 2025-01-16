a1c783e82f832c6c8dc8bba6c12a29461499f53f
System32 <stvgmpl@gmail.com>
2025-01-16 13:57:28 +0100

    Revert "Added verbose"
    This reverts commit 65c54732746be150d2264077bd0a8c72c1cf0027.

--- termux-color.sh
+++ termux-color.sh
@@ -1,12 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/bash
 # termux-color - Change the colorscheme of Termux
 # License: Unlicense
-TERMUXCOLOR_VERSION="1.3"
-COLORFILE_PATH="$HOME/.termux/colors.properties"
 
 show_usage()
 {
-	echo "usage: termux-color [-hvV] [-c colorscheme] [-C colorscheme] ..."
+	echo "usage: termux-color [-hv] [-c colorscheme] ..."
 	exit 0
 }
 
@@ -15,10 +13,7 @@ show_usage()
 # exists.
 copy_colorscheme()
 {
-	cp $PREFIX/share/termux-color/colors/${SELECTED_COLORSCHEME} ${COLORFILE_PATH}
-	if [ "$VERBOSE" == "1" ]; then
-		echo "[info] Copying colorscheme ${SELECTED_COLORSCHEME} to ${COLORFILE_PATH}"
-	fi
+	cp $PREFIX/share/termux-color/colors/${SELECTED_COLORSCHEME} $HOME/.termux/colors.properties
 }
 
 #
@@ -26,35 +21,17 @@ copy_colorscheme()
 #
 if [ "$1" == "-h" ]; then
 	show_usage
-	if [ "$VERBOSE" == "1" ]; then
-		echo "[info] Showed usage"
-	fi
-	exit 0
 elif [ "$1" == "-v" ]; then
-	echo "termux-color (termux-color) v$TERMUXCOLOR_VERSION"
+	echo "termux-color (termux-color) v1.1"
 	echo "License: Unlicense <https://unlicense.org>"
 	echo "This software comes with NO WARRANTY, to the extent permitted by law."
 	echo
 	echo "Written by System32."
-	if [ "$VERBOSE" == "1" ]; then
-		echo "[info] Showed version information"
-	fi
 	exit 0
 elif [ "$1" == "-c" ]; then
 	SELECTED_COLORSCHEME="$2"
 	copy_colorscheme
 	exit 0
-elif [ "$1" == "-C" ]; then
-	SELECTED_COLORSCHEME="$2"
-	copy_colorscheme
-	termux-reload-settings
-	exit 0
-elif [ "$1" == "-V" ]; then
-	VERBOSE=1
-	if [ "$VERBOSE" == "1" ]; then
-		echo "termux-color v$TERMUXCOLOR_VERSION"
-		echo "[info] Enabled being verbose"
-	fi
 fi
 
 #
@@ -66,27 +43,14 @@ restart_termux()
 {
 	echo "Restarting Termux ..."
 	termux-reload-settings
-	if [ "$VERBOSE" == "1" ]; then
-		echo "[info] Restarted termux."
-	fi
 }
 # List of colorschemes.
 color_list()
 {
-	if [ "$VERBOSE" == "1" ]; then
-		echo "[info] Showing list of colorschemes ..."
-	fi
 	echo "[0] default"
 	echo "[1] xterm_dark"
 	echo "[2] monochrome"
 	echo "[3] default_green"
-}
-# Lines to execute when being verbose.
-verbose_selected_colorscheme()
-{
-	if [ "$VERBOSE" == "1" ]; then
-		echo -e "[info] Selected colorscheme \'${SELECTED_COLORSCHEME}\'"
-	fi
 }
 
 #
@@ -101,22 +65,18 @@ read COLOR_OPTION
 # aforementioned functions.
 if [ "$COLOR_OPTION" = "0" ]; then
 	SELECTED_COLORSCHEME="default"
-	verbose_selected_colorscheme
 	copy_colorscheme
 	restart_termux
 elif [ "$COLOR_OPTION" = "1" ]; then
 	SELECTED_COLORSCHEME="xterm_dark"
-	verbose_selected_colorscheme
 	copy_colorscheme
 	restart_termux
 elif [ "$COLOR_OPTION" = "2" ]; then
 	SELECTED_COLORSCHEME="monochrome"
-	verbose_selected_colorscheme
 	copy_colorscheme
 	restart_termux
 elif [ "$COLOR_OPTION" = "3" ]; then
 	SELECTED_COLORSCHEME="default_green"
-	verbose_selected_colorscheme
 	copy_colorscheme
 	restart_termux
 fi
