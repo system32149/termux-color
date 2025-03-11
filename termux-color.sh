3706a06d97671153c435842eb5d3e9f14ec14432
System32 <stvgmpl@gmail.com>
2025-03-11 16:27:49 +0000

    Use case statements

--- termux-color.sh
+++ termux-color.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 # termux-color - Change the colorscheme of Termux
 # License: Unlicense
-VERSION="1.6"
+VERSION="1.6.1"
 
 show_usage()
 {
@@ -20,35 +20,37 @@ copy_colorscheme()
 #
 # Options
 #
-if [ "$1" == "-v" ]; then
-	echo "termux-color (termux-color) v$VERSION"
-	echo "License: Unlicense <https://unlicense.org>"
-	echo "This software comes with NO WARRANTY, to the extent permitted by law."
-	echo
-	echo "Written by System32."
-	exit 0
-elif [ "$1" == "-c" ]; then
-	SELECTED_COLORSCHEME="$2"
-	test -f ${PREFIX}/share/termux-color/colors/${2}
-	if [ "$?" == "1" ]; then
-		echo "termux-color: invalid colorscheme ${2}"
-		exit 2
-	else
-		copy_colorscheme
-		exit 0
-	fi
-elif [ "$1" == "-C" ]; then
-	SELECTED_COLORSCHEME="$2"
-	test -f ${PREFIX}/share/termux-color/colors/${2}
-	if [ "$?" == "1" ]; then
-		echo "termux-color: invalid colorscheme ${2}"
-		exit 2
-	else
-		copy_colorscheme
-		termux-reload-settings
-		exit 0
-	fi
-elif [ $# -gt 0 ]; then
+case $1 in
+	"-v")
+		echo "termux-color (termux-color) v$VERSION"
+		echo "License: Unlicense <https://unlicense.org>"
+		echo "This software comes with NO WARRANTY, to the extent permitted by law."
+		echo
+		echo "Written by System32."
+		exit 0 ;;
+	"-c")
+		SELECTED_COLORSCHEME="$2"
+		test -f ${PREFIX}/share/termux-color/colors/${2}
+		if [ "$?" == "1" ]; then
+			echo "termux-color: invalid colorscheme ${2}"
+			exit 2
+		else
+			copy_colorscheme
+			exit 0
+		fi ;;
+	"-C")
+		SELECTED_COLORSCHEME="$2"
+		test -f ${PREFIX}/share/termux-color/colors/${2}
+		if [ "$?" == "1" ]; then
+			echo "termux-color: invalid colorscheme ${2}"
+			exit 2
+		else
+			copy_colorscheme
+			termux-reload-settings
+			exit 0
+		fi ;;
+esac
+if [ $# -gt 0 ]; then
 	show_usage
 	exit 1
 fi
@@ -81,21 +83,21 @@ read -p "Select option: " COLOR_OPTION
 
 # Set the $selected_colorscheme variable and execute the
 # aforementioned functions.
-if [ "$COLOR_OPTION" = "0" ]; then
-	SELECTED_COLORSCHEME="default"
-	copy_colorscheme
-	restart_termux
-elif [ "$COLOR_OPTION" = "1" ]; then
-	SELECTED_COLORSCHEME="xterm_dark"
-	copy_colorscheme
-	restart_termux
-elif [ "$COLOR_OPTION" = "2" ]; then
-	SELECTED_COLORSCHEME="monochrome"
-	copy_colorscheme
-	restart_termux
-elif [ "$COLOR_OPTION" = "3" ]; then
-	SELECTED_COLORSCHEME="default_green"
-	copy_colorscheme
-	restart_termux
-fi
-
+case $COLOR_OPTION in
+	"0")
+		SELECTED_COLORSCHEME="default"
+		copy_colorscheme
+		restart_termux ;;
+	"1")
+		SELECTED_COLORSCHEME="xterm_dark"
+		copy_colorscheme
+		restart_termux ;;
+	"2")
+		SELECTED_COLORSCHEME="monochrome"
+		copy_colorscheme
+		restart_termux ;;
+	"3")
+		SELECTED_COLORSCHEME="default_green"
+		copy_colorscheme
+		restart_termux ;;
+esac
